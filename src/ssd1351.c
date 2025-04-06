3d380d58e02f21c0e7c14c6fe204139ba034ccab
nktsb <neymarus@yandex.ru>
2025-04-06 20:00:58 +0300

    Api developing, added SPI functions

--- src/ssd1351.c
+++ src/ssd1351.c
@@ -9,30 +9,206 @@
 
 #include "ssd1351.h"
 
+#include <string.h>
 #include <zephyr/device.h>
-#include <zephyr/drivers/mipi_dbi.h>
-#include <zephyr/pm/device.h>
-#include <zephyr/sys/byteorder.h>
+#include <zephyr/init.h>
 #include <zephyr/drivers/display.h>
+#include <zephyr/drivers/gpio.h>
+#include <zephyr/drivers/spi.h>
+#include <zephyr/kernel.h>
 
 #define LOG_LEVEL CONFIG_DISPLAY_LOG_LEVEL
 #include <zephyr/logging/log.h>
 LOG_MODULE_REGISTER(display_ssd1351);
 
+typedef bool (*ssd1351_bus_ready_fn)(const struct device *dev);
+typedef int (*ssd1351_write_bus_fn)(const struct device *dev, uint8_t *buf, size_t len,
+				   bool command);
+
 struct ssd1351_config {
 	struct spi_dt_spec spi;
-	struct gpio_dt_spec data_cmd_gpio;
+	ssd1351_bus_ready_fn bus_ready;
+	ssd1351_write_bus_fn write_bus;
+	struct gpio_dt_spec data_cmd;
 	struct gpio_dt_spec reset_gpio;
 	uint16_t width;
 	uint16_t height;
 };
 
+struct ssd1351_data {
+	uint16_t xres;
+	uint16_t yres;
+
+	enum display_pixel_format pixel_format;
+	enum display_orientation orientation;
+};
+
+static bool ssd1351_bus_ready_spi(const struct device *dev)
+{
+	const struct ssd1351_config *config = dev->config;
+
+	if (gpio_pin_configure_dt(&config->data_cmd, GPIO_OUTPUT_INACTIVE) < 0) {
+		return false;
+	}
+
+	return spi_is_ready_dt(&config->bus.spi);
+}
+
+static int ssd1351_write_bus_spi(const struct device *dev, uint8_t *buf, size_t len, bool command)
+{
+	const struct ssd1351_config *config = dev->config;
+	int ret;
+
+	gpio_pin_set_dt(&config->data_cmd, command ? 0 : 1);
+	struct spi_buf tx_buf = {.buf = buf, .len = len};
+
+	struct spi_buf_set tx_bufs = {.buffers = &tx_buf, .count = 1};
+
+	ret = spi_write_dt(&config->bus.spi, &tx_bufs);
+
+	return ret;
+}
+
+static int ssd1351_init(const struct device *dev)
+{
+	const struct ssd1351_config *cfg = dev->config;
+
+	if (cfg->rotation == 0) {
+		data->xres = cfg->width;
+		data->yres = cfg->height;
+		data->orientation = DISPLAY_ORIENTATION_NORMAL;
+	} else if (cfg->rotation == 90) {
+		data->xres = cfg->height;
+		data->yres = cfg->width;
+		data->orientation = DISPLAY_ORIENTATION_ROTATED_90;
+	} else if (cfg->rotation == 180) {
+		data->xres = cfg->width;
+		data->yres = cfg->height;
+		data->orientation = DISPLAY_ORIENTATION_ROTATED_180;
+	} else if (cfg->rotation == 270) {
+		data->xres = cfg->height;
+		data->yres = cfg->width;
+		data->orientation = DISPLAY_ORIENTATION_ROTATED_270;
+	}
+
+}
+
+static int ssd1351_blanking_on(const struct device *dev)
+{
+	// TODO blanking on command
+}
+
+static int ssd1351_blanking_off(const struct device *dev)
+{
+	// TODO blanking off command
+}
+
+static int ssd1351_write(const struct device *dev, const uint16_t x, const uint16_t y,
+		const struct display_buffer_descriptor *desc, const void *buf)
+{
+	const struct ssd1351_config *config = dev->config;
+	// switch(config->pixel_format)
+	// {} TODO handle two rgb formats
+}
+
+static int ssd1351_set_brightness(const struct device *dev, const uint8_t brightness)
+{
+	// TODO set brightness
+}
+
+static int ssd1351_set_contrast(const struct device *dev, const uint8_t contrast)
+{
+	// TODO set contrast
+}
+
+static void ssd1351_get_capabilities(const struct device *dev, struct display_capabilities *caps)
+{
+	const struct ssd1351_config *config = dev->config;
+	struct ssd1351_data *data = dev->data;
+
+	memset(capabilities, 0, sizeof(struct display_capabilities));
+	capabilities->x_resolution = config->width;
+	capabilities->y_resolution = config->height;
+
+	capabilities->supported_pixel_formats = PIXEL_FORMAT_RGB_565 | PIXEL_FORMAT_RGB_888;
+
+	capabilities->current_pixel_format = data->pixel_format;
+	capabilities->current_orientation = data->orientation;
+}
+
+static int ssd1351_set_pixel_format(const struct device *dev,
+			     const enum display_pixel_format pixel_format)
+{
+	const struct ssd1351_config *config = dev->config;
+	struct ssd1351_data *data = dev->data;
+	config->pixel_format = pixel_format;
+
+	return 0;
+}
+
+static int ssd1351_set_orientation(const struct device *dev,
+			    const enum display_orientation orientation)
+{
+	struct ssd1351_data *data = dev->data;
+
+	data->orientation = orientation;
+
+	if (orientation == DISPLAY_ORIENTATION_NORMAL)
+	{
+		data->xres = cfg->width;
+		data->yres = cfg->height;
+	} 
+	else
+	if (orientation == DISPLAY_ORIENTATION_ROTATED_90)
+	{
+		data->xres = cfg->height;
+		data->yres = cfg->width;
+	}
+	else
+	if (orientation == DISPLAY_ORIENTATION_ROTATED_180)
+	{
+		data->xres = cfg->width;
+		data->yres = cfg->height;
+	}
+	else
+	if (orientation == DISPLAY_ORIENTATION_ROTATED_270)
+	{
+		data->xres = cfg->height;
+		data->yres = cfg->width;
+	}
+
+
+	// TODO write orientation command
+}
 
 static DEVICE_API(display, ssd1351_api) = {
 	.blanking_on = ssd1351_blanking_on,
 	.blanking_off = ssd1351_blanking_off,
 	.write = ssd1351_write,
+	.set_brightness = ssd1351_set_brightness,
+	.set_contrast = ssd1351_set_contrast,
 	.get_capabilities = ssd1351_get_capabilities,
 	.set_pixel_format = ssd1351_set_pixel_format,
 	.set_orientation = ssd1351_set_orientation,
 };
+
+
+#define SSD1351_DEFINE(node_id)															\
+	static const struct ssd1351_config ssd1351_config_##node_id = {						\
+		.spi = SPI_DT_SPEC_GET(															\
+				node_id, SPI_OP_MODE_MASTER | SPI_TRANSFER_MSB | SPI_WORD_SET(8), 0)	\
+		.bus_ready = ssd1351_bus_ready_spi,												\
+		.write_bus = ssd1351_write_bus_spi,												\
+		.data_cmd = GPIO_DT_SPEC_GET_OR(node_id, dc_gpios, {0}),						\
+		.reset_gpio = GPIO_DT_SPEC_GET_OR(node_id, reset_gpios, {0}),					\
+		.height = DT_PROP(node_id, height),												\
+		.width = DT_PROP(node_id, width),												\	
+	};																					\
+	static struct ssd1351_data ssd1351_data_##inst = {									\
+		.pixel_format = DT_INST_PROP(inst, pixel_format),								\
+	};																					\
+	DEVICE_DT_DEFINE(node_id, ssd1351_init, NULL, &ssd1351_data_##inst,					\
+			&ssd1351_config_##node_id, POST_KERNEL, CONFIG_DISPLAY_INIT_PRIORITY,		\
+			&ssd1351_api);
+
+DT_INST_FOREACH_STATUS_OKAY(SSD1351_DEFINE)
