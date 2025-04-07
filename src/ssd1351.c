392176ad35a87c9680b70bde77819dee44d3fb33
nktsb <neymarus@yandex.ru>
2025-04-08 00:03:15 +0300

    color swap fix

--- src/ssd1351.c
+++ src/ssd1351.c
@@ -79,10 +79,10 @@ static int ssd1351_set_remap(const struct device *dev)
 	switch (data->pixel_format)
 	{
 		case PIXEL_FORMAT_RGB_888:
-			remap_mask |= 0b10000000;
+			remap_mask |= 0b10000100;
 			break;
 		case PIXEL_FORMAT_BGR_565:
-			remap_mask |= 0b01000100;
+			remap_mask |= 0b01000000;
 			break;
 		case PIXEL_FORMAT_RGB_565:
 		default:
